4efe4c86ec51d522b11e4bff6279e0d977d8953d
poznianski <poznianski@gmail.com>
2022-10-14 14:50:01 +0300

    implemented business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>
-      Button
-    </div>
+    <button type="button" className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}>
+      Get Started
+    </button>
   )
 }
 
