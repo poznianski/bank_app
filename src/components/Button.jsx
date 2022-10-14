fb0ce9be7af112d7840c0f83194ed7f5b1167892
poznianski <poznianski@gmail.com>
2022-10-14 21:00:51 +0300

    implemented CardDeal and Testimonials

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}>
+    <button type="button" className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient rounded-[10px] ${styles}`}>
       Get Started
     </button>
   )
